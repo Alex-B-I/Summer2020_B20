bc6fcdb718a8ed8db7a1d0462edb10d19f0c29f1
Alex-B-I <alex.berloh@gmail.com>
2020-09-16 17:12:10 -0500

    Commit

--- src/Office_Hours/Practice_09_16_2020/ThrowsKeyword.java
+++ src/Office_Hours/Practice_09_16_2020/ThrowsKeyword.java
@@ -2,8 +2,24 @@ package Office_Hours.Practice_09_16_2020;
 
 public class ThrowsKeyword {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        ThrowsKeyword obj = new ThrowsKeyword();
+        obj.methodA();
+        obj.methodA();
+        obj.methodA();
+     /*
+        try{
+            obj.methodA();
+        }catch (InterruptedException e){}
+        try{
+            obj.methodA();
+        }catch (InterruptedException e){}
+        try{
+            obj.methodA();
+        }catch (InterruptedException e){}
+
+      */
     }
 
     public void methodA()throws InterruptedException{
