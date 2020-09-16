51339e88864b3f4b555bcc39e1d1e7a672209be2
Alex-B-I <alex.berloh@gmail.com>
2020-09-16 17:06:18 -0500

    throws keyword

---
+++ src/Office_Hours/Practice_09_16_2020/ThrowsKeyword.java
@@ -0,0 +1,13 @@
+package Office_Hours.Practice_09_16_2020;
+
+public class ThrowsKeyword {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void methodA()throws InterruptedException{
+        Thread.sleep(3000);
+    }
+
+}
