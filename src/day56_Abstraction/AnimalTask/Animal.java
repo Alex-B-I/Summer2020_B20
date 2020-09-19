0ca61c9541d6bfe3623f58b25d1fdb2cbc767b3b
Alex-B-I <alex.berloh@gmail.com>
2020-09-19 11:40:49 -0500

    Commit

---
+++ src/day56_Abstraction/AnimalTask/Animal.java
@@ -0,0 +1,6 @@
+package day56_Abstraction.AnimalTask;
+
+public abstract class Animal {
+    public abstract void eat();
+    public abstract void sleep();
+}
---
+++ src/day56_Abstraction/AnimalTask/Flyable.java
@@ -0,0 +1,6 @@
+package day56_Abstraction.AnimalTask;
+
+public interface Flyable {
+    boolean hasWings = true;
+    public abstract void fly();
+}
