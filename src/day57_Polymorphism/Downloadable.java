57eb677d689212a2aa685936a8b7122ba94da553
Alex-B-I <alex.berloh@gmail.com>
2020-09-23 18:45:02 -0500

    Commit

---
+++ src/day57_Polymorphism/Downloadable.java
@@ -0,0 +1,12 @@
+package day57_Polymorphism;
+/*
+Warmup task:
+    1. create an interface named Downloadable:
+            abstrct method: download();
+ */
+
+public interface Downloadable {
+
+    void download();
+
+}
