a0402affee0cbc272fd1fa35c935a27fab861ebf
Alex-B-I <alex.berloh@gmail.com>
2020-09-23 19:07:22 -0500

    Commit

---
+++ src/day57_Polymorphism/Phone.java
@@ -0,0 +1,33 @@
+package day57_Polymorphism;
+/*
+4. create an abstract class named Phone
+            instance variables: brand, model, price, size
+            abstract methods: texting(), calling()
+            add constructor that can initialize the fields
+            if the price is set to negative, the system should throw an exception with a message of:
+                    Invalid Price, cannot be negative
+ */
+public abstract class Phone {
+    public String brand;
+    public String model;
+    public double price;
+    public String size;
+
+    public Phone(String brand, String model, double price, String size) {
+        this.brand = brand;
+        this.model = model;
+        if(price <= 0){
+            throw new RuntimeException("Price of the phone can not be negative or equal to zero! ! !");
+        }
+        this.price = price;
+        this.size = size;
+    }
+
+    public abstract void texting(long phoneNumber);
+
+    public abstract void calling(long phoneNumber);
+
+
+
+
+}
---
+++ src/day57_Polymorphism/iPhone.java
@@ -0,0 +1,44 @@
+package day57_Polymorphism;
+
+/*
+ 5. create a class named iPhone that can inherit from AppleApp and Phone
+            actions: texting(), calling(), faceTiming()
+            if the price of Iphoen is more than 1500, the system should throw an exception with a message of:
+                    Invalid Price, Iphone' price cannot more than 1500
+
+ */
+    public class iPhone extends Phone implements AppleApp, Downloadable{
+
+        public iPhone(String brand, String model, double price, String size) {
+            super("iPhone", model, price, size);
+            if(price>1500){
+                throw new RuntimeException("Invalid Price, Iphone' price cannot more than 1500");
+            }
+        }
+
+        @Override
+        public void texting(long phoneNumber) {
+            System.out.println("iPhone is texting: "+phoneNumber);
+        }
+
+        @Override
+        public void calling(long phoneNumber) {
+            System.out.println("iPhone is calling: " +phoneNumber);
+        }
+
+        @Override
+        public void download() {
+            System.out.println("iPhone is downloading app from "+AppStoreName);
+        }
+
+    @Override
+    public String toString() {
+        return "iPhone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=$" + price +
+                ", size='" + size + '\'' +
+                '}';
+    }
+}
+
