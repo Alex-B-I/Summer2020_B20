71c5f0ede5b589d7041ebbc2f6c9e3da8430f83a
Alex-B-I <alex.berloh@gmail.com>
2020-10-03 13:36:54 -0500

    Commit

---
+++ src/day60_Collection/SetInterface.java
@@ -0,0 +1,22 @@
+package day60_Collection;
+
+import java.util.*;
+
+public class SetInterface {
+    public static void main(String[] args) {
+        Set <Integer> s1 = new HashSet<>();
+        s1.addAll(Arrays.asList(8,5,4,3,2,10,7,1)); //does not keep the insertion order
+        s1.addAll(Arrays.asList(8,8,8,8,8,8,8,8,8)); //does not accept duplicates
+
+        System.out.println("HashSet: "+s1);
+
+        Set<Integer> s2 = new LinkedHashSet<>();
+        s2.addAll(Arrays.asList(8,5,4,3,2,10,7,1));// keeps the insertion order as it is
+        s2.addAll(Arrays.asList(8,8,8,8,8,8,8,8,8)); //does not accept duplicates
+        System.out.println("Linked HashSet: "+s2);
+
+        Set<Integer> s3 =new TreeSet<>();
+
+
+    }
+}
