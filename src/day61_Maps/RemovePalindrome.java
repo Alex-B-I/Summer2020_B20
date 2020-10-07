55e6d564b42d1fe1a4423d29040a557f9a7a3519
Alex-B-I <alex.berloh@gmail.com>
2020-10-07 18:29:11 -0500

    Commit

---
+++ src/day61_Maps/RemovePalindrome.java
@@ -0,0 +1,39 @@
+package day61_Maps;
+
+import Library.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+/*
+1. write a program that can remove the palindrome string from a list of String
+			DO NOT use Lambda expressions
+ */
+public class RemovePalindrome {
+    public static void main(String[] args) {
+
+        String[]words = {"Java", "Python", "Level", "Kayak", "Cybertek", "Zaman", "Ana", "Batch20"};
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList(words));
+        System.out.println(list);
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            String each = it.next();
+            //String reverse = Util.revers(each);
+            String reverse = "";
+            for (int i =each.length()-1;i>=0;i--){
+                reverse+=each.charAt(i);
+            }
+            //System.out.println(each+" : "+reverse);
+            if(each.equalsIgnoreCase(reverse)){
+                it.remove();
+            }
+
+        }
+        System.out.println(list);
+
+    }
+
+
+}
