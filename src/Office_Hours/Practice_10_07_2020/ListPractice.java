274eb3128e2d9323da742853dc96cb35cda3dc20
Alex-B-I <alex.berloh@gmail.com>
2020-10-07 16:43:59 -0500

    Commit

--- src/Office_Hours/Practice_10_07_2020/ListPractice.java
+++ src/Office_Hours/Practice_10_07_2020/ListPractice.java
@@ -13,10 +13,33 @@ public class ListPractice {
         List<String> list1 = new Vector<>();
         list1.addAll(Arrays.asList("Nurbiye", "Emine", "Dawud", "Emine", "Emine"));
         System.out.println(list1);
+        System.out.println(list1.get(2));
 
         List<String> list2 = new Stack<>();
         list2.addAll(Arrays.asList("Nurbiye", "Emine", "Dawud", "Emine", "Emine"));
         System.out.println(list2);
+        System.out.println(list2.get(2));
+
+        System.out.println("=====================");
+
+        Stack<String> stack = new Stack<>();
+        stack.addAll(list);
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
+
+        stack.pop(); //LIFO
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
 
 
     }
