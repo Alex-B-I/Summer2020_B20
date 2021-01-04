d4ab87e92847c8465f7e32114aad16fe7451ae58
Alex-B-I <alex.berloh@gmail.com>
2021-01-04 10:17:28 -0600

    BootCamp Array

--- src/bootCamp/ArrayPractice.java
+++ src/bootCamp/ArrayPractice.java
@@ -3,8 +3,27 @@ package bootCamp;
 import java.util.Arrays;
 
 public class ArrayPractice {
+    /*
+    Arrays: size is fixed
+    primitives & objects both supported
+
+    Wrapper Class
+
+     */
+
     public static void main(String[] args) {
         int [] array = new int[5];
+        array[0]=1;
+        array[1]=2;
+        array[2]=3;
+        array[3]=4;
+        array[4]=5;
+        System.out.println(Arrays.toString(array));
+
+     //   array[5]=6;
+
+        array = new int[3];
+
         System.out.println(Arrays.toString(array));
     }
 }
